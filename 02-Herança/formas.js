fab93e620f2f9410968b028b489093965423f657
Gabriel <gabrielmmve13@gmail.com>
2024-03-22 20:53:13 -0300

    Herança

--- 02-Herança/formas.js
+++ 02-Herança/formas.js
@@ -1,14 +1,26 @@
 //esta classe abstrata reune caracteristicas de classes concretas(classes que agrupam caracteristicas do mundo real) (com classes abstratas nao se pode instanciar)
 class Forma {
 
-    constructor() {
-        this.tipoDeForma = 'Forma Abstrata';
+    // definindo um valor padrão para caso nao seja passado nenhum parametro
+    constructor(tipoDeForma = 'FormaAbstrata') {
+        this.tipoDeForma = tipoDeForma;
+        // this.tipoDeForma = 'Forma Abstrata';
     }
 
     // ao estar dentro o escopo de uma classe, ja é subentendido que é uma função, nao precisa da palavra reservada "function"
     imprimeForma() {
-        alert( `Tipo de forma: ${this.tipoDeForma} - Área: ${this.calculaArea()}`);
-        // alert(`Tipo de forma: ${this.tipoDeForma}`)
+
+        /* estrutura de try - catch, estrutura de tratamento de erro */
+        // try (do ingles, 'tente'), executar este codigo (dentro do escopo de try)
+        try {
+            alert( `Tipo de forma: ${this.tipoDeForma} - Área: ${this.calculaArea()}`);
+            console.log("codigo funcionou");
+        } 
+        /* catch (pegue) - pegue o erro, caso, de qualquer tipo de erro dentro do try, ele executara oq esta dentro do escopo de catch, se nao der nenhum tipo de erro no escopo do try, ele nao sera executado - ao inves de so parar a execução do programa ao encontrar um erro, sera passada esta mensagem tratada */        
+        catch (error) {
+            alert("O código está com erro");
+        }
+       
     }
 
     // throw: jogar, new: novo, cria uma novo objeto da classe erro e passo o parametro: `O método calculaArea() deve ser implementado na subclasse.` 
--- 02-Herança/quadrado.js
+++ 02-Herança/quadrado.js
@@ -2,9 +2,12 @@ class Quadrado extends Forma{
 
     constructor(lado){
         // entra no constructor da classe pai (Forma) e herda seus atributos
-        super();
+        // super();
         // utilizando atributo da classe pai (Forma), e definindo o tipo de forma, quadrado
-        this.tipoDeForma = 'Quadrado';
+        // this.tipoDeForma = 'Quadrado';
+
+        // acessando um membro da classe pai (Forma) no caso o constructor da classe forma, passando como parametro 'quadrado' e atribui no 'tipoDeForma'
+        super("Quadrado");
         // this.lado (atributo), é um membro da classe pois possui: "this."
         this.lado = lado;
     }
